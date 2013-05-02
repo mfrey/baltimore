2309192a7fa22145bfd0f2f0ad56fa6c407aa6d3
Michael Frey <mail@mfrey.net>
2013-05-02 07:42:35 +0200

    added vector file example

--- analysis/packetdeliveryrateanalysis.py
+++ analysis/packetdeliveryrateanalysis.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python2.7
 
 class PacketDeliveryRateAnalysis:
+  # TODO: build up a string instead of a simple print
+  def __str__(self):
+	print "Sent Packets:     ", self.sent
+
+	if self.sent > 0:
+	  print "Received Packets: ", self.received, " ", (self.received/self.sent * 100), "%" 
+	else:
+	  print "Received Packets: ", self.received 
+
+	print "Route Failures:   ", self.routeFailures
+
+
   def __init__(self):
 	self.sent = 0
 	self.received = 0
@@ -8,7 +20,6 @@ class PacketDeliveryRateAnalysis:
 	self.routeFailures = 0
 
   def evaluate(self, nodes):
-	
 	for node in nodes:
 	  try:
 		self.sent = self.sent + int(nodes[node].results["trafficSent"])
@@ -18,8 +29,7 @@ class PacketDeliveryRateAnalysis:
 	  except KeyError:
 		print "unknown key "
 
-	print "Sent Packets:     ", self.sent
-	print "Received Packets: ", self.received, " ", (self.received/self.sent * 100), "%" 
-	print "Route Failures:   ", self.routeFailures
+
+
 
 
--- plot/packetdeliveryrateplot.py
+++ plot/packetdeliveryrateplot.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python2.7
 
+import matplotlib.pyplot as plt
+
 class PacketDeliveryRatePlot:
   def draw(self, filename):
 	plt.figure()
 	plt.plot([0,10,50,80,90,100], [10,10,8,7,6,0], drawstyle="steps", color=(0./256,55./256,108./256), lw=2.5)
 	#plt.axis([0, 110, 0, 11])
-	plt.ylabelself.ylabel,va="center",ha="center")
+	plt.ylabel(self.ylabel,va="center",ha="center")
 	plt.xlabel(self.xlabel)
 	plt.savefig(filename)
 
@@ -14,5 +16,16 @@ class PacketDeliveryRatePlot:
 	self.xlabel = "Pause Time [sec]"
 
 
+  def test(self):
+	plt.figure()
+	plt.plot([0,10,50,80,90,100], [10,10,8,7,6,0], drawstyle="line", marker="s", color=(0./256,55./256,108./256), lw=2.5)
+	plt.ylabel(self.ylabel,va="center",ha="center")
+	plt.xlabel(self.xlabel)
+	plt.grid(axis="y")
+	plt.savefig('test.png')
+
+
+foo = PacketDeliveryRatePlot()
+foo.test()
 
 
