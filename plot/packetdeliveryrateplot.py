6dee65e27330e4db642c4cc64e7503705f8a28b5
Michael Frey <frey@informatik.hu-berlin.de>
2013-05-02 16:55:31 +0200

    small changes to plot for packet delivery rate

--- plot/packetdeliveryrateplot.py
+++ plot/packetdeliveryrateplot.py
@@ -3,22 +3,17 @@
 import matplotlib.pyplot as plt
 
 class PacketDeliveryRatePlot:
-  def draw(self, filename):
-	plt.figure()
-	plt.plot([0,10,50,80,90,100], [10,10,8,7,6,0], drawstyle="steps", color=(0./256,55./256,108./256), lw=2.5)
-	#plt.axis([0, 110, 0, 11])
-	plt.ylabel(self.ylabel,va="center",ha="center")
-	plt.xlabel(self.xlabel)
-	plt.savefig(filename)
-
   def __init__(self):
 	self.ylabel = "Delivery Rate [%]"
 	self.xlabel = "Pause Time [sec]"
+        self.xlist = []
+        self.ylist = []
 
 
-  def test(self):
+  def draw(self, filename):
 	plt.figure()
-	plt.plot([0,10,50,80,90,100], [10,10,8,7,6,0], drawstyle="line", marker="s", color=(0./256,55./256,108./256), lw=2.5)
+        for index, value in enumerate(self.xlist):
+	  plt.plot(value, self.ylist[index], drawstyle="line", marker="s", color=(0./256,55./256,108./256), lw=2.5)
 	plt.ylabel(self.ylabel,va="center",ha="center")
 	plt.xlabel(self.xlabel)
 	plt.grid(axis="y")
