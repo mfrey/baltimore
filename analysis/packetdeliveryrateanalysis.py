2d344a899445b516c8dc8ce36012d38117052fd3
Friedrich Große <friedrich.grosse@gmail.com>
2013-05-03 11:03:24 +0200

    Simplified the formatting of the analysis output

--- analysis/packetdeliveryrateanalysis.py
+++ analysis/packetdeliveryrateanalysis.py
@@ -3,6 +3,9 @@
 class PacketDeliveryRateAnalysis:
 
   def __init__(self):
+    self.clear()
+
+  def clear(self):
     self.sent = 0
     self.received = 0
     self.routeFailures = 0
@@ -13,12 +16,13 @@ class PacketDeliveryRateAnalysis:
 
   def evaluate(self, nodes):
     for node in nodes:
-      self.sent = self.sent + int(self.get_metric(nodes, node, "trafficSent"))
-      self.received = self.received + int(self.get_metric(nodes, node, "trafficReceived"))
-      self.routeFailures = self.routeFailures + int(self.get_metric(nodes, node, "routeFailure:count"))
-      self.routingLoopDetected = self.routingLoopDetected + int(self.get_metric(nodes, node, "routingLoopDetected:count"))
-      self.routeDiscoveryFailed = self.routeDiscoveryFailed + int(self.get_metric(nodes, node, "packetUnDeliverable:count"))
-      self.timeToLiveExpired = self.timeToLiveExpired + int(self.get_metric(nodes, node, "dropZeroTTLPacket:count"))
+      self.sent += int(self.get_metric(nodes, node, "trafficSent"))
+      self.received += int(self.get_metric(nodes, node, "trafficReceived"))
+      self.routeFailures += int(self.get_metric(nodes, node, "routeFailure:count"))
+      self.routingLoopDetected += int(self.get_metric(nodes, node, "routingLoopDetected:count"))
+      self.routeDiscoveryFailed += int(self.get_metric(nodes, node, "packetUnDeliverable:count"))
+      self.timeToLiveExpired += int(self.get_metric(nodes, node, "dropZeroTTLPacket:count"))
+    
     self.inexplicableLoss = self.sent - self.received - self.routingLoopDetected - self.routeFailures - self.routeDiscoveryFailed - self.timeToLiveExpired
 
     if self.inexplicableLoss < 0:
@@ -33,31 +37,27 @@ class PacketDeliveryRateAnalysis:
       print "unknown key ", metric, " for node ", identifier 
       
   def __str__(self):
-    result = "Sent Packets:              " + str(self.sent) + "\n"
-    result = result + "Received Packets:          " + str(self.received) + ", " + self.get_percentage(self.received)
-    result = result + "Route Failures:            " + str(self.routeFailures) + ", " + self.get_percentage(self.routeFailures)
-    result = result + "Failed Route Discoveries:  " + str(self.routeDiscoveryFailed) + ", " + self.get_percentage(self.routeDiscoveryFailed)
-    result = result + "Dropped Packets (TTL = 0): " + str(self.timeToLiveExpired) + ", " + self.get_percentage(self.timeToLiveExpired) 
-    result = result + "Inexplicable loss:         " + str(self.inexplicableLoss) + ", " + self.get_percentage(self.inexplicableLoss) 
+    result  = self.get_result_line("Sent Packets", self.sent)
+    result += self.get_result_line("Received Packets", self.received)
+    result += self.get_result_line("Route Failures", self.routeFailures)
+    result += self.get_result_line("Failed Route Discoveries", self.routeDiscoveryFailed)
+    result += self.get_result_line("Dropped Packets (TTL = 0)", self.timeToLiveExpired) 
+    result += self.get_result_line("Inexplicable loss", self.inexplicableLoss)
+    result += "\n"
+    result += "Packet Delivery Rate was %.2f%%" % self.get_packet_delivery_rate();
     return result
 
-  def get_percentage(self, value):
+  def get_result_line(self, name, value):
     if self.sent > 0:
-      result = (float(value)/float(self.sent))*100.0
-      return str("{0:.2f}".format(round(result, 2))) + " % \n" 
-    return "\n"
+      percent = "%6.2f%%" % ((float(value)/float(self.sent)) * 100.0)
+    else:
+      percent = "  0.00%%"
+    
+    name += ":"
+    return "%(name)-26s %(value)3d\t" % {"name": name, "value": value} + percent + "\n" 
 
   def get_packet_delivery_rate(self):
     if self.sent > 0:
       result = (float(self.received)/float(self.sent))*100.0
-      return "{0:.2f}".format(round(result, 2)) 
+      return round(result, 2) 
     return 0.0
-
-  def clear(self):
-    self.sent = 0
-    self.received = 0
-    self.routeFailures = 0
-    self.routingLoopDetected = 0
-    self.routeDiscoveryFailed = 0
-    self.timeToLiveExpired = 0
-    self.inexplicableLoss = 0
