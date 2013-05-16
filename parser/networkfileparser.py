24d5c675501e7571cf8bcee0595dcc9cd5275310
Michael Frey <frey@inf.fu-berlin.de>
2013-05-16 15:16:15 +0200

    added method for determining if a node is connected to another node (for drawing networks)

--- parser/networkfileparser.py
+++ parser/networkfileparser.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2.7
 
+from math import pow, sqrt
 import networkx as nx
 
 class NetworkFileParser:
@@ -10,10 +11,32 @@ class NetworkFileParser:
         with open(file_path) as f:
             for line in f:
                 self._parse_line(line)
+
+        self._create_connections()
    
     def _parse_line(self, line):
         entry = line.strip().split()
-        self._create_node(entry[0], entry[1], entry[2])
+        self._create_node(entry[0], float(entry[1]), float(entry[2]), float(entry[3]))
 
-    def _create_node(self, node_identifier, x_position, y_position):
-        self.network.add_node(node_identifier, pos=(x_position, y_position))
+    def _create_node(self, node_identifier, x_position, y_position, node_radius):
+        self.network.add_node(node_identifier, pos=(x_position, y_position),radius=node_radius)
+
+    def _create_connections(self):
+        positions = nx.get_node_attributes(self.network, 'pos')
+        radius = nx.get_node_attributes(self.network, 'radius')
+
+        for current_node in self.network.nodes():
+            current_node_x = positions[current_node][0]
+            current_node_y = positions[current_node][1]
+            current_node_radius = radius[current_node]
+
+            for next_node in self.network.nodes():
+                if current_node != next_node:
+                    next_node_x = positions[next_node][0]
+                    next_node_y = positions[next_node][1]
+
+                    distance = sqrt(pow(current_node_x - next_node_x, 2) + pow(current_node_x - next_node_x, 2))
+
+                    if distance <= current_node_radius:
+                        self.network.add_edge(current_node, next_node)
+                
