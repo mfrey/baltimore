f2af6bbcd9c198fa9243405a119130657472681d
Michael Frey <mail@mfrey.net>
2013-05-20 15:37:54 +0200

    added additional parameters for the runner script

--- experiment/runner.py
+++ experiment/runner.py
@@ -5,13 +5,14 @@ import os
 from subprocess import call
 
 class Runner(object):
-  def __init__(self, scenario, repetition, ned_path, omnetpp_ini, binary, ld_library_path):
+  def __init__(self, scenario, repetition, ned_path, omnetpp_ini, binary, ld_library_path, cwd):
 	self.scenario = scenario
 	self.repetition = repetition
 	self.ned_path = ned_path
 	self.omnetpp_ini = omnetpp_ini
 	self.binary = binary
 	self.ld_library_path = ld_library_path
+	self.cwd = cwd
 
   @classmethod
   def run_simulation(cls, args):
@@ -22,16 +23,17 @@ class Runner(object):
 	environment = dict(os.environ)
 	environment["LD_LIBRARY_PATH"] = self.ld_library_path
 
-	call([self.binary, "-r", self.repetition, "-u", "Cmdenv", "-c", self.scenario, "-n", self.ned_path, self.omnetpp_ini], env=environment)
+	call([self.binary, "-r", self.repetition, "-u", "Cmdenv", "-c", self.scenario, "-n", self.ned_path, self.omnetpp_ini], env=environment, cwd=self.cwd)
 
+# this is just for testing purposes and will be removed soon
 if __name__ == "__main__":
   binary = "/home/michael/Desktop/Projekte/code/ara-sim/omnetpp/ara-sim"
   repetition = "1"
   scenario = "midSize"
   omnetpp_ini = "/home/michael/Desktop/Projekte/code/ara-sim/simulations/static/omnetpp.ini"
-  ned_path = "/home/michael/Desktop/Projekte/code/ara-sim/inetmanet/src:/home/michael/Desktop/Projekte/code/ara-sim/inetmanet/examples:/home/michael/Desktop/Projekte/code/ara-sim/:/home/michael/Desktop/Projekte/code/ara-sim/omnetpp"
+  ned_path = "/home/michael/Desktop/Projekte/code/ara-sim/inetmanet/src:/home/michael/Desktop/Projekte/code/ara-sim/inetmanet/examples:/home/michael/Desktop/Projekte/code/ara-sim/simulations/static:/home/michael/Desktop/Projekte/code/ara-sim/omnetpp"
   ld_library_path = "$LD_LIBRARY_PATH:/home/michael/Desktop/Projekte/code/ara-sim/src:/home/michael/Desktop/Projekte/code/ara-sim/inetmanet/src"
-
-  runner = Runner(scenario, repetition, ned_path, omnetpp_ini, binary, ld_library_path)
+  cwd = "/home/michael/Desktop/Projekte/code/ara-sim/simulations/static"
+  runner = Runner(scenario, repetition, ned_path, omnetpp_ini, binary, ld_library_path, cwd)
   runner.run()
 
