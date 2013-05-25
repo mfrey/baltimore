7114fcb03795e64aaa789aaf5796d70d5d563fc7
Friedrich Große <friedrich.grosse@gmail.com>
2013-05-25 10:45:14 +0200

    Some cleanup for the arguments: long versions only for the boolean flags

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ from experiment.experimentmanager import ExperimentManager
 
 def main():
     parser = argparse.ArgumentParser(description='baltimore - an evaluation script for the ara-sim framework')
-    parser.add_argument('-c', '--config', dest='configuration', type=str, default="", action='store', help='a configuarion file for baltimore')
-    parser.add_argument('-d', '--dir', dest='directory', type=str, default="", action='store', help='a directory which contains OMNeT++ result files')
+    parser.add_argument('-c', dest='configuration', type=str, default="", action='store', help='a configuarion file for baltimore')
+    parser.add_argument('-d', dest='directory', type=str, default="", action='store', help='a directory which contains OMNeT++ result files')
+    parser.add_argument('-s', dest='scenario', type=str, default="", action='store', help="evaluate a specific scenario")
     parser.add_argument('-v', '--verbose', dest='verbose', default=False, const=True, action='store_const', help="print out verbose information for each iteration")
     parser.add_argument('-n', '--network', dest='network', default=False, const=True, action='store_const', help="draw network graph for scenario(s)")
-    parser.add_argument('-s', '--scenario', dest='scenario', type=str, default="", action='store', help="evaluate a specific scenario")
     parser.add_argument('-j', '--json', dest='json', type=str, default="", action='store', help="specify location for json export")
     parser.add_argument('-r', '--run', dest='run', default=False, const=True, action='store_const', help="first run the simulations as specified via the configuration then analyse the results")
     arguments = parser.parse_args()
