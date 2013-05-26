a1df598932ac94a27b94f9407ace9481e379c2cd
Michael Frey <mail@mfrey.net>
2013-05-26 19:23:52 +0200

    obsolete changes to class Runner

--- experiment/runner.py
+++ experiment/runner.py
@@ -18,21 +18,8 @@ class Runner(object):
 	self.ld_library_path = settings['ld_library_path']
 	self.cwd = settings['cwd']
 
-
   def run(self):
 	environment = dict(os.environ)
 	environment["LD_LIBRARY_PATH"] = self.ld_library_path
 	call([self.binary, "-r", str(self.repetition), "-u", "Cmdenv", "-c", self.scenario, "-n", self.ned_path, self.omnetpp_ini], env=environment, cwd=self.cwd)
-
-# this is just for testing purposes and will be removed soon
-#if __name__ == "__main__":
-#  binary = "/home/michael/Desktop/Projekte/code/ara-sim/omnetpp/ara-sim"
-#  repetition = "1"
-#  scenario = "midSize"
-#  omnetpp_ini = "/home/michael/Desktop/Projekte/code/ara-sim/simulations/static/omnetpp.ini"
-#  ned_path = "/home/michael/Desktop/Projekte/code/ara-sim/inetmanet/src:/home/michael/Desktop/Projekte/code/ara-sim/inetmanet/examples:/home/michael/Desktop/Projekte/code/ara-sim/simulations/static:/home/michael/Desktop/Projekte/code/ara-sim/omnetpp"
-#  ld_library_path = "$LD_LIBRARY_PATH:/home/michael/Desktop/Projekte/code/ara-sim/src:/home/michael/Desktop/Projekte/code/ara-sim/inetmanet/src"
-#  cwd = "/home/michael/Desktop/Projekte/code/ara-sim/simulations/static"
-#  runner = Runner(scenario, repetition, ned_path, omnetpp_ini, binary, ld_library_path, cwd)
-#  runner.run()
 
