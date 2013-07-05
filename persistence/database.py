541e11736e90dd2fd81842835dd634c5497dc805
Michael Frey <frey@inf.fu-berlin.de>
2013-07-05 17:24:50 +0200

    added more functionality to class Database

--- persistence/database.py
+++ persistence/database.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python2.7
 
 from pymongo import MongoClient
+from pymongo.errors import PyMongoError
 
 class Database:
-    def __init__(self, user, password, host, database):
-        self.database = database
+    def __init__(self, user, password, database, host, port=27017):
         self.user = user
         self.password = password
+        self.database_name = database
         self.host = host
+        self.port = port
+
+    def open(self):
+        self.client = MongoClient(self.host, self.port)
+        self.database = self.client[self.database_name]
+        try:
+            self.database.authenticate(self.user, self.password)
+        except PyMongoError: 
+            print "user and/or password unknown"
+ 
+    def close(self):
+        self.client.close()
+
+    def add_experiment(self, experiment):
+        self.database['experiments'].insert(experiment)
    
 #    def add_experiment(self, experiment):
-        
+      
+if __name__ == "__main__":
+     database = Database("baltimore_admin", "<add password>", "baltimore", "localhost")
+     database.open()
+
