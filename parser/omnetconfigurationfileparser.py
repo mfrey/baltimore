7663bd0c28bfa0c94208fad29e612b2539a06df5
Michael Frey <frey@inf.fu-berlin.de>
2013-07-05 16:23:32 +0200

    added 'extends' support for scenarios in class OMNetConfigurationFileParser

--- parser/omnetconfigurationfileparser.py
+++ parser/omnetconfigurationfileparser.py
@@ -10,13 +10,25 @@ class OMNeTConfigurationFileParser:
     def get_section(self, section):
         return self.omnetpp_ini_file.items(section)
 
-    def get_scenario(self, scenario):
-        return self.get_section("Config " + scenario)
+    def get_scenario(self, scenario, result):
+        configuration = "Config " + scenario
+
+        if any("extends" in setting for setting in self.get_section(configuration)):
+            base_scenario = self.omnetpp_ini_file.get(configuration, "extends")
+            # the scenario itself might be a extended scenario, so get the other settings as well
+            self.get_scenario(base_scenario, result)
+
+        for data in self.get_section(configuration):
+            result[data[0]] = data[1]
+
+        return result
 
 if __name__ == "__main__":
-    configuration = OMNeTConfiguration("/vol/home-vol1/simulant/frey/Desktop/Projekte/code/ara-sim/simulations/static/omnetpp.ini")
+    configuration = OMNeTConfigurationFileParser("/home/frey/Desktop/Projekte/code/ara-sim/simulations/ara/omnetpp.ini")
     for setting in configuration.get_section("General"):
         print setting[0], " = ", setting[1]
 
-    for setting in configuration.get_scenario("midStatic"):
-        print setting[0], " = ", setting[1]
+    result = configuration.get_scenario("ARA600", {})
+
+    for key, value in result.items():
+        print key, " = ", value
