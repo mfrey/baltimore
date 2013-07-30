86ae9c54616a4ecf7a0b2dd01cf07dac30d15db3
Michael Frey <frey@inf.fu-berlin.de>
2013-07-30 11:13:46 +0200

    fixed a naming issue introduced with the previous commit

--- analysis/lastpacketanalysis.py
+++ analysis/lastpacketanalysis.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2.7
 
 import sys
+import logging
 import os.path
 import numpy as np
 
@@ -12,6 +13,10 @@ from plot.boxplot import BoxPlot
 class LastPacketAnalysis(Analysis):
     def __init__(self, scenario, location):
         Analysis.__init__(self, scenario, location, "last_packet")
+
+        self.logger = logging.getLogger('baltimore.analysis.LastPacketAnalysis')
+        self.logger.debug('creating an instance of LastPacketAnalysis for scenario %s', scenario)
+
         self.data_min = -1
         self.data_max = -1
         self.data_median = -1
@@ -19,14 +24,17 @@ class LastPacketAnalysis(Analysis):
         self.data_avg = -1
 
     def evaluate(self, experiment_results, is_verbose=False):
-        print "\nRunning arrival of last packet analysis.."
+        self.logger.info("running arrival of last packet analysis")
+
         raw_data = [experiment_results.get_metric("timeOfLastReceivedPacket", repetition) for repetition in experiment_results.repetitions]
         data = [element for element in raw_data if element != 0]
 
-        self.data_min = np.amin(result)
-        self.data_max = np.amax(result)
-        self.data_median = np.median(result)
-        self.data_std = np.std(result)
-        self.data_avg = np.average(result)
+        self.data_min = np.amin(data)
+        self.data_max = np.amax(data)
+        self.data_median = np.median(data)
+        self.data_std = np.std(data)
+        self.data_avg = np.average(data)
+
+        self.logger.info("last packet: %d, %d, %d, %d, %d [min, max, median, std, avg] for scenario %s", self.data_min, self.data_max, self.data_median, self.data_std, self.data_avg, self.scenario)
 
         self.plot_boxplot("Arrival of Last Packet", "", "Arrival Time [ms]", data)
