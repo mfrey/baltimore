267947d80ebb29971f9afc7aae61cf2cd4541d29
Michael Frey <frey@inf.fu-berlin.de>
2013-07-30 11:06:20 +0200

    added min,max,median,std,avg metrics to class LastPacketAnalysis

--- analysis/lastpacketanalysis.py
+++ analysis/lastpacketanalysis.py
@@ -4,29 +4,29 @@ import sys
 import os.path
 import numpy as np
 
+from analysis import Analysis
+
 from plot.lineplot import LinePlot
 from plot.boxplot import BoxPlot
 
-class LastPacketAnalysis:
+class LastPacketAnalysis(Analysis):
     def __init__(self, scenario, location):
-        self.data = []
-        self.scenario = scenario
-        self.location = location
+        Analysis.__init__(self, scenario, location, "last_packet")
+        self.data_min = -1
+        self.data_max = -1
+        self.data_median = -1
+        self.data_std = -1
+        self.data_avg = -1
 
     def evaluate(self, experiment_results, is_verbose=False):
         print "\nRunning arrival of last packet analysis.."
         raw_data = [experiment_results.get_metric("timeOfLastReceivedPacket", repetition) for repetition in experiment_results.repetitions]
-        self.data = [element for element in raw_data if element != 0]
-        self._create_boxplot()
-        
+        data = [element for element in raw_data if element != 0]
 
-    def _create_boxplot(self):
-        plot = BoxPlot()
-        plot.title = "Arrival of Last Packet"
-        plot.xlabel = ""
-        plot.ylabel = "Arrival Time [ms]"
-        plot.draw(self.data, os.path.join(self.location, self.scenario + "_last_packet.png"))
+        self.data_min = np.amin(result)
+        self.data_max = np.amax(result)
+        self.data_median = np.median(result)
+        self.data_std = np.std(result)
+        self.data_avg = np.average(result)
 
-
-
-    
+        self.plot_boxplot("Arrival of Last Packet", "", "Arrival Time [ms]", data)
