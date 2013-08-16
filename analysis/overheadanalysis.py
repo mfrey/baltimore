11bc5833388a7d18a6ded12dcb9155e92fa3a2dc
Friedrich Große <friedrich.grosse@gmail.com>
2013-08-16 16:39:47 +0200

    Overhead analysis does now create bit overhead csv as well
    Also we do not calculate and write the 5% and 95% percentile to the csv file

--- analysis/overheadanalysis.py
+++ analysis/overheadanalysis.py
@@ -4,6 +4,7 @@ import sys
 import logging
 
 import numpy as np
+from scipy import stats
 
 from analysis import Analysis
 
@@ -12,11 +13,13 @@ class OverheadAnalysis(Analysis):
         Analysis.__init__(self, scenario, location, "overhead", repetitions, csv)
         self.logger = logging.getLogger('baltimore.analysis.OverheadAnalysis')
         self.logger.debug('creating an instance of OverheadAnalysis for scenario %s', scenario)
-        self.overhead = []
+        self.packet_overhead = []
+        self.bit_overhead = []
+        self.packet_statistics = {}
+        self.bit_statistics = {}
 
     def evaluate(self, experiment_results, is_verbose=False):
         self.logger.info("running overhead analysis..")
-        # TODO printout single repetition data
         self.analyse_average_values(experiment_results)
         self._get_overhead(experiment_results)
 
@@ -29,18 +32,32 @@ class OverheadAnalysis(Analysis):
            data_packets = results.get_metric('nrOfDataPackets', repetition)
            control_packets = results.get_metric('nrOfControlPackets', repetition)
            all_packets = data_packets + control_packets
-           self.overhead.append(control_packets/all_packets)
+           self.packet_overhead.append(control_packets/all_packets)
+
+           nr_of_data_bits = results.get_metric('nrOfSentDataBits', repetition)
+           nr_of_control_bits = results.get_metric('nrOfSentControlBits', repetition)
+           nr_of_all_bits = nr_of_data_bits + nr_of_control_bits
+           self.bit_overhead.append(nr_of_control_bits/nr_of_all_bits)
 
     def _get_overhead(self, results):
-        if len(self.overhead) == 0:
+        if len(self.packet_overhead) == 0:
             self._compute_overhead(results)
 
-        self.data_min = np.amin(self.overhead)
-        self.data_max = np.amax(self.overhead)
-        self.data_median = np.median(self.overhead)
-        self.data_std = np.std(self.overhead)
-        self.data_avg = np.average(self.overhead)
-           
+        self.packet_statistics['Min'] = round(np.amin(self.packet_overhead)*100, 2)
+        self.packet_statistics['Max'] = round(np.amax(self.packet_overhead)*100, 2)
+        self.packet_statistics['Med'] = round(np.median(self.packet_overhead)*100, 2)
+        self.packet_statistics['Std'] = round(np.std(self.packet_overhead)*100, 2)
+        self.packet_statistics['Avg'] = round(np.average(self.packet_overhead)*100, 2)
+	self.packet_statistics['low'] = round(stats.scoreatpercentile(self.packet_overhead, 5)*100, 2)
+        self.packet_statistics['hig'] = round(stats.scoreatpercentile(self.packet_overhead, 95)*100, 2)
+        
+        self.bit_statistics['Min'] = round(np.amin(self.bit_overhead)*100, 6)
+        self.bit_statistics['Max'] = round(np.amax(self.bit_overhead)*100, 6)
+        self.bit_statistics['Med'] = round(np.median(self.bit_overhead)*100, 6)
+        self.bit_statistics['Avg'] = round(np.average(self.bit_overhead)*100, 6)
+        self.bit_statistics['Std'] = round(np.std(self.bit_overhead)*100, 6)
+        self.bit_statistics['low'] = round(stats.scoreatpercentile(self.bit_overhead, 5)*100, 6)
+        self.bit_statistics['hig'] = round(stats.scoreatpercentile(self.bit_overhead, 95)*100, 6)
 
     def analyse_average_values(self, results):
         self.print_analysis_header(results)
@@ -82,9 +99,16 @@ class OverheadAnalysis(Analysis):
         print "%-34s %*d   %s   %s   %s   %s   %s" % (name, nr_of_digits, average_metric, percent, median, std_deviation, min, max)
 
     def export_csv(self):
-        file_name = self.scenario + "_" + self.metric + "_aggregated.csv"
+        file_name = self.scenario + "_" + self.metric + "_packets.csv"
 	disclaimer = [['#'],['#'], ['# ' + str(self.date) + ' - overhead (packets) for scenario ' + self.scenario],['# aggregated over ' + str(self.repetitions) + ' repetitions'],['#']]
-	header = ['min', 'max', 'median', 'std', 'avg']
-	data = [[self.data_min, self.data_max, self.data_median, self.data_std,  self.data_avg]]
-
+	header = ['min', 'max', 'median', 'std', 'avg', 'low', 'high']
+	data = [[self.packet_statistics['Min'], self.packet_statistics['Max'], self.packet_statistics['Med'], self.packet_statistics['Std'], self.packet_statistics['Avg'], self.packet_statistics['low'], self.packet_statistics['hig']]]
         self._write_csv_file(file_name, disclaimer, header, data)
+
+        # Write bit overhead to csv
+        file_name = self.scenario + "_" + self.metric + "_bits.csv"
+        disclaimer = [['#'],['#'], ['# ' + str(self.date) + ' - overhead (bits) for scenario ' + self.scenario],['# aggregated over ' + str(self.repetitions) + ' repetitions'],['#']]
+        header = ['minimum', 'maximum', 'median', 'deviation', 'average', 'low', 'high']
+        data = [[self.bit_statistics['Min'], self.bit_statistics['Max'], self.bit_statistics['Med'], self.bit_statistics['Std'], self.bit_statistics['Avg'], self.bit_statistics['low'], self.bit_statistics['hig']]]
+        self._write_csv_file(file_name, disclaimer, header, data)
+
