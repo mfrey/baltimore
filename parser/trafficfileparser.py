93d6af6a580064a910f93915c91dbe13766e0258
Michael Frey <frey@inf.fu-berlin.de>
2013-11-07 17:55:45 +0100

    added traffic file parser

---
+++ parser/trafficfileparser.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2.7
+
+from lxml import etree
+
+class TrafficFileParser: 
+
+    def get_traffic_settings(self, file_name, traffic_id):
+        search_expression = "//config/[@id='" + str(traffic_id) + "']"
+        document = etree.parse(file_name)
+        result = document.findall(search_expression)
+
+        if len(result) == 1:
+           return result[0].attrib
+       
+        return {}
